0269811cc9fd86179d81820762cdd9e8e19c233f
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-07-27 19:55:46 -0300

    feat: configura autenticacao JWT com SimpleJWT e endpoints de token

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('agenda.urls')),
     path('api/', include('clinicas.urls')),
     path('api/', include('pacientes.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
