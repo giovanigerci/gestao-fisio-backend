f06cec7e6cc840676347c10b8619f811f1c13e1a
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-07-31 19:31:00 -0300

    feat: implementa filtro de periodo e total geral ao resumo financeiro

--- financeiro/views.py
+++ financeiro/views.py
@@ -1,20 +1,35 @@
-from django.db.models import Count, F
+from django.db.models import Count, F, Sum
+from django.db.models.functions import TruncWeek, TruncMonth
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from agenda.models import Agendamento
 
 class ResumoFinanceiroView(APIView):
     def get(self, request):
+        periodo = request.query_params.get('periodo', 'mes')
+
+        if periodo == 'semana':
+            trunc_func = TruncWeek('data')
+        else: 
+            trunc_func = TruncMonth('data')
+
         agendamentos = Agendamento.objects.filter(
             eh_gratuito=False,
             profissional=request.user.profissional
         )
 
-        resumo = agendamentos.values(
-            'clinica', 'clinica__nome'
+        resumo = agendamentos.annotate(
+            periodo=trunc_func
+        ).values(
+           'periodo', 'clinica', 'clinica__nome'
         ).annotate(
             total_atendimentos=Count('id'),
             receita_total=F('clinica__valor_por_atendimento') * Count('id'),
-        )
+        ).order_by('periodo')
 
-        return Response(resumo)
+        total_geral = sum(item['receita_total'] for item in resumo)
+
+        return Response({
+            'por_clinica': resumo,
+            'total_geral': total_geral,
+        })
