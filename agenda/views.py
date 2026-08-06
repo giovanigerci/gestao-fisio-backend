b8275361cc2c4b0ed6236c65570abd3e16cbea23
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-08-06 20:04:22 -0300

    feat: adiciona filtro de periodo em agendamentos e preenchimento automatico do profissional

--- agenda/views.py
+++ agenda/views.py
@@ -6,7 +6,15 @@ class AgendamentoViewSet(viewsets.ModelViewSet):
     serializer_class = AgendamentoSerializer
 
     def get_queryset(self):
-        return Agendamento.objects.filter(profissional=self.request.user.profissional)
+        queryset = Agendamento.objects.filter(profissional=self.request.user.profissional)
+
+        data_inicio = self.request.query_params.get('data_inicio')
+        data_fim = self.request.query_params.get('data_fim')
+
+        if data_inicio and data_fim:
+            queryset = queryset.filter(data__gte=data_inicio, data__lte=data_fim)
+        
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(profissional=self.request.user.profissional)
