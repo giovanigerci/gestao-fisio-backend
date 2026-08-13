77a5649750797975f819e022fd3d3504c7c391a2
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-08-13 20:15:12 -0300

    fix: trata token invalido no cookie como nao autenticado, evitando 401 indevido

--- profissionais/authentication.py
+++ profissionais/authentication.py
@@ -1,11 +1,16 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         access_token = request.COOKIES.get('access_token')
 
-        if access_token is None:
+        if not access_token:
             return None
 
-        validated_token = self.get_validated_token(access_token)
+        try:
+            validated_token = self.get_validated_token(access_token)
+        except (InvalidToken, TokenError):
+            return None
+
         return self.get_user(validated_token), validated_token
