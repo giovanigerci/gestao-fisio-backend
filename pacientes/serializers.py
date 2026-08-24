aa04936f423d803499972a16cbb8e1e9bc837024
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-08-24 20:32:44 -0300

    feat: adiciona validação de data de nascimento no serializer

--- pacientes/serializers.py
+++ pacientes/serializers.py
@@ -8,7 +8,6 @@ class PacienteSerializer(serializers.ModelSerializer):
     total_sessoes = serializers.SerializerMethodField()
     status = serializers.SerializerMethodField()
 
-    
     class Meta:
         model = Paciente
         fields = ['id', 'profissional', 'nome', 'cpf', 'telefone', 'email', 'data_nascimento', 'endereco', 'historico_medico',
@@ -27,3 +26,12 @@ class PacienteSerializer(serializers.ModelSerializer):
         tem_recente = Agendamento.objects.filter(paciente=obj, status='RE', data__gte=limite).exists()
         tem_futuro = Agendamento.objects.filter(paciente=obj, status='AG', data__gte=date.today()).exists()
         return 'Ativo' if (tem_recente or tem_futuro) else 'Inativo'
+
+    def validate_data_nascimento(self, value):
+        if value:
+            if value > date.today():
+                raise serializers.ValidationError("Data de nascimento não pode ser maior que a data atual.")
+            limite_antigo = date.today().replace(year=date.today().year - 120)
+            if value < limite_antigo:
+                raise serializers.ValidationError("Data de nascimento inválida.")
+        return value
